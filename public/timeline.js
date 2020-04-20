45f523d9e7f6751d089a126b855a4f9ac76a8e1f
kfriendship1003 <kfriendship94@gmail.com>
2020-04-20 14:31:03 +0900

    :construction: add getSchedules, render function

--- public/timeline.js
+++ public/timeline.js
@@ -3,7 +3,7 @@ let travels = [];
 const $dateList = document.querySelector('.date-list');
 const $scheduleList = document.querySelector('.schedule-list');
 
-const toggleActiveDate = (target) => {
+const toggleActiveDate = target => {
   if (!target.matches('.date-list > li') && !target.matches('.date-list > li > div')) return;
   [...$dateList.children].forEach(date => date.classList.toggle('active', (target === date || target.parentNode === date)));
 };
@@ -15,13 +15,22 @@ $scheduleList.onclick = ({ target }) => {
   const id = target.parentNode.id;
 };
 
+const getSchedules = async () => {
+  const { data } = await axios.get('/travels');
+  travels = data;
+  console.log(travels[0].schedule);
+  render();
+};
+
 const render = () => {
   let html = '';
+  let travel = travels[0].schedule;
+  console.log(travel);
 
-  travels.forEach(({id, time, place, detail}) => {
+  travel.forEach(({ id, timeFrom, place, detail }) => {
     html = `<li class="schedule" id="s${id}">
-            <div class="time">${time}</div>
-            <div class="place">${place}}</div>
+            <div class="time">${timeFrom}</div>
+            <div class="place">${place}</div>
             <div class="detail">${detail}</div>
             <div class="remove-btn">X</div>
           </li>`;
@@ -30,9 +39,4 @@ const render = () => {
   $scheduleList.innerHTML = html;
 };
 
-
-const dataBase = axios.get('/travels')
-    .then(({ data }) => travels = data)
-    .then((travels) => console.log(travels))
-    .catch(err => console.error(err));
-
+getSchedules();
