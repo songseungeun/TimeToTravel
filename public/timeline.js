65ab0cfd280446cfe314666712fbcfe89697b0f5
kfriendship1003 <kfriendship94@gmail.com>
2020-04-20 14:06:00 +0900

    :triangular_flag_on_post: Add toggleActiveDate function

--- public/timeline.js
+++ public/timeline.js
@@ -1,14 +1,38 @@
-const $dateList = document.querySelector('.date-list');
+let travels = [];
 
-$dateList.onclick = ({ target }) => {
+const $dateList = document.querySelector('.date-list');
+const $scheduleList = document.querySelector('.schedule-list');
+
+const toggleActiveDate = (target) => {
   if (!target.matches('.date-list > li') && !target.matches('.date-list > li > div')) return;
   [...$dateList.children].forEach(date => date.classList.toggle('active', (target === date || target.parentNode === date)));
 };
 
-let promise = '';
+$dateList.addEventListener('click', ({ target }) => toggleActiveDate(target));
+
+$scheduleList.onclick = ({ target }) => {
+  if (!target.matches('.schedule-list > .remove-btn')) return;
+  const id = target.parentNode.id;
+};
+
+const render = () => {
+  let html = '';
+
+  travels.forEach(({id, time, place, detail}) => {
+    html = `<li class="schedule" id="s${id}">
+            <div class="time">${time}</div>
+            <div class="place">${place}}</div>
+            <div class="detail">${detail}</div>
+            <div class="remove-btn">X</div>
+          </li>`;
+  });
+
+  $scheduleList.innerHTML = html;
+};
+
 
 const dataBase = axios.get('/travels')
-    .then(({ data }) => promise = data)
-    .then((promise) => console.log(promise))
+    .then(({ data }) => travels = data)
+    .then((travels) => console.log(travels))
     .catch(err => console.error(err));
 
