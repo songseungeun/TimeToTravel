b1d810e115669b72e06d845065f70c0c6306c8e1
kfriendship1003 <kfriendship94@gmail.com>
2020-04-20 20:50:08 +0900

    :triangular_flag_on_post: Add closing and opening popup feature

--- public/js/travel-list.js
+++ public/js/travel-list.js
@@ -1,18 +1,40 @@
-// const newTravelBtn = document.querySelector('.new-travel-btn');
-// const popupBg = document.querySelector('.new-travel-popup-bg');
-// const popupRemove = document.querySelector('.new-travel-popup-remove-btn');
-// const newTravelPopup = document.querySelector('.new-travel-popup');
+// DOMs
+const $newTravelBtn = document.querySelector('.new-travel-btn');
+const $addTravelBtn = document.querySelector('.add-travel-btn');
+const $popupBg = document.querySelector('.new-travel-popup-bg');
+const $popupRemove = document.querySelector('.new-travel-popup-remove-btn');
+const $newTravelPopup = document.querySelector('.new-travel-popup');
+const $inputTitle = document.querySelector('.input-title');
+const $inputPlace = document.querySelector('.input-place');
+const $selectYear = $newTravelPopup.querySelector('#year-select');
+const $selectMonth = $newTravelPopup.querySelector('#month-select');
+const $selectDate = $newTravelPopup.querySelector('#day-select');
 
-// function openPopup() {
-//   popupBg.style.display = 'block';
-//   newTravelPopup.style.display = 'block';
-// }
+// functions
+const closeHomePopup = () => {
+  $newTravelPopup.style.display = 'none';
+  $popupBg.style.display = 'none';
+};
 
-// function closePopup() {
-//   popupBg.style.display = 'none';
-//   newTravelPopup.style.display = 'none';
-// }
+function openHomePopup() {
+  $popupBg.style.display = 'block';
+  $newTravelPopup.style.display = 'block';
+};
 
-// newTravelBtn.addEventListener('click', openPopup);
-// popupBg.addEventListener('click', closePopup);
-// popupRemove.addEventListener('click', closePopup);
+// event handlers
+$newTravelBtn.addEventListener('click', openHomePopup);
+$popupBg.addEventListener('click', closeHomePopup);
+$popupRemove.addEventListener('click', closeHomePopup);
+
+$addTravelBtn.onclick = () => {
+  const title = $inputTitle.value.trim();
+  const place = $inputPlace.value.trim();
+
+  console.log(title)
+  console.log(place)
+
+  // const year = selectYear.options[selectYear.selectedIndex].text;
+  // console.log(year);
+  // console.log(selectYear.value)
+
+}
