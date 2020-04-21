b1ea452cf0489d6cc28d0184317214d305d4b662
kfriendship1003 <kfriendship94@gmail.com>
2020-04-21 16:51:28 +0900

    :truck: move codes to main.js

--- public/js/travel-list.js
+++ public/js/travel-list.js
@@ -1,25 +1,13 @@
 // import
-import { getSchedules, schedules } from './timeline.js';
-
-// state
-let travels = [];
-let timelineOf = '';
+// import { getSchedules, schedules } from './timeline.js';
 
 // DOMs
-const $travelList = document.querySelector('.travel-list');
+
 const $newTravelBtn = document.querySelector('.new-travel-btn');
-const $addTravelBtn = document.querySelector('.add-travel-btn');
 const $popupBg = document.querySelector('.new-travel-popup-bg');
 const $popupRemove = document.querySelector('.new-travel-popup-remove-btn');
 const $newTravelPopup = document.querySelector('.new-travel-popup');
-const $inputTitle = document.querySelector('.input-title');
-const $inputPlace = document.querySelector('.input-place');
-const $startYear = $newTravelPopup.querySelector('#start-year-select');
-const $startMonth = $newTravelPopup.querySelector('#start-month-select');
-const $startDate = $newTravelPopup.querySelector('#start-day-select');
-const $endYear = $newTravelPopup.querySelector('#end-year-select');
-const $endMonth = $newTravelPopup.querySelector('#end-month-select');
-const $endDate = $newTravelPopup.querySelector('#end-day-select');
+
 
 // functions
 const closeTravelPopup = () => {
@@ -32,83 +20,11 @@ const openTravelPopup = () => {
   $newTravelPopup.style.display = 'block';
 };
 
-const generateId = () => travels.length ? Math.max(...travels.map(({ id }) => id)) + 1 : 1;
-
-const renderTravelList = () => {
-  let html = '';
-
-  travels.forEach(({ id, title, place, startDate, endDate }) => {
-    html += ` <li id=${id}>
-          <h2>${title}</h2>
-          <em>D-${startDate}</em>
-          <div class="travel-info">
-            <span class="travel-place">${place}</span>
-            <span class="travel-date">${startDate} ~ ${endDate}</span>
-          </div>
-          <div class="travel-remove-btn">X</div>
-        </li>`;
-  });
-
-  $travelList.innerHTML = html;
-};
-
-const getTravels = async () => {
-  const { data } = await axios.get('/travels');
-  travels = data;
-  renderTravelList();
-};
-
-const removeTravel = async (target) => {
-  if (!target.matches('.travel-list > li > .travel-remove-btn')) return;
-  const id = target.parentNode.id;
-
-  await axios.delete(`/travels/${id}`);
-  travels = travels.filter((travel) => travel.id !== +id);
-  renderTravelList();
-};
-
-const goToTimeline = (target) => {
-  if (!target.matches('.travel-list > li')) return;
-
-  timelineOf = target.id;
-  getSchedules(timelineOf);
-};
-
-const makeScheduleData = newId => {
-  axios.post('/schedules', { id: newId });
-};
-
 // event handlers
-window.onload = getTravels;
 
 $newTravelBtn.addEventListener('click', openTravelPopup);
 $popupBg.addEventListener('click', closeTravelPopup);
 $popupRemove.addEventListener('click', closeTravelPopup);
 
-$addTravelBtn.onclick = async () => {
-  const title = $inputTitle.value.trim();
-  const place = $inputPlace.value.trim();
-  const startDate = `${$startYear.value}/${$startMonth.value}/${$startDate.value}`;
-  const endDate = `${$endYear.value}/${$endMonth.value}/${$endDate.value}`;
-  const newId = generateId();
-
-  const { data } = await axios.post('/travels', { id: newId, title, place, startDate, endDate });
-  travels = [data, ...travels];
-
-  closeTravelPopup();
-  renderTravelList();
-  makeScheduleData(newId);
-
-  $inputTitle.value = '';
-  $inputPlace.value = '';
-
-  [...$newTravelPopup.children].forEach(child => {
-    if (child.nodeName === 'SELECT') child.firstElementChild.selected = 'selected';
-  });
-};
-
-$travelList.addEventListener('click', ({ target }) => removeTravel(target));
-$travelList.addEventListener('click', ({ target }) => goToTimeline(target));
-
 // export
-export { timelineOf };
+// export { timelineOf };
