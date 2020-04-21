5d3f64ac67476c8186ac3b2f0cc1ec917bf638f5
heejinlee07 <heejinlee07@gmail.com>
2020-04-21 12:29:43 +0900

    merged file

--- public/travel-list-popup.js
+++ public/travel-list-popup.js
@@ -3,8 +3,6 @@ const $newSchedulePopUp = document.querySelector(".new-schedule-popup");
 const $popupBg = document.querySelector(".popup-bg");
 const $addScheduleBtn = document.querySelector(".add-schedule-btn");
 const $popupRemoveBtn = document.querySelector(".popup-remove-btn");
-const $selectDate = document.querySelector(".select-date");
-const $selectStartHour = document.querySelector(".select-start-hour");
 
 const $monthSelect = document.querySelector("#month-select");
 const $dateSelect = document.querySelector("#date-select");
@@ -12,11 +10,14 @@ const $startHourSelect = document.querySelector("#start-hour-select");
 const $startMinSelect = document.querySelector("#start-min-select");
 const $endHourSelect = document.querySelector("#end-hour-select");
 const $endMinSelect = document.querySelector("#end-min-select");
-const $inputPlace = document.querySelector(".input-place");
-const $inputDetail = document.querySelector(".input-detail");
 
-const $mainSchedule = document.querySelector(".main-schedule");
-const $warningText = document.querySelector(".warnning-text");
+const $inputPlace = document.querySelector("#schedule-input-place");
+const $inputDetail = document.querySelector("#schedule-input-detail");
+const $selectDateWarning = document.querySelector("#date-warning-label");
+const $selectStartWarning = document.querySelector("#start-warning-label");
+const $selectEndWarning = document.querySelector("#end-warning-label");
+const $inputDetailWarning = document.querySelector("#detail-warning-label");
+const $inputPlaceWarning = document.querySelector("#place-warning-label");
 
 const closePopup = () => {
   $newSchedulePopUp.style.display = "none";
@@ -32,17 +33,48 @@ $newScheduleBtn.onclick = () => {
 //x버튼을 누르면 팝업창 종료
 $popupRemoveBtn.addEventListener("click", closePopup);
 
-function insertDate() {
-  const $monthSelect = document.querySelector("#month-select");
-
-  const selectValue = $monthSelect.options[$monthSelect.selectedIndex];
-}
-
-//경고 메세지
-$addScheduleBtn.onclick = (e) => {
-  if ($selectDate.value === "none") {
-    $warningText.style.display = "block";
-  }
+window.onclick = ({ target }) => {
+  if (target !== $popupBg || target === $newScheduleBtn) return;
+  closePopup();
 };
 
-//test
+//경고문구
+//FIXME: &&나 ||로 세트인 아이들을 묶어줄 수 없는지?
+
+function showWarning(element, defaultValue, warningElement) {
+  if (element.value === defaultValue) {
+    warningElement.classList.remove("text-hidden");
+    return 1;
+  } else {
+    warningElement.classList.add("text-hidden");
+    return 0;
+  }
+}
+
+$addScheduleBtn.onclick = () => {
+  let errorCount = 0;
+  errorCount += showWarning($inputPlace, "", $inputPlaceWarning);
+  errorCount += showWarning($inputDetail, "", $inputDetailWarning);
+  errorCount += showWarning($monthSelect, "default", $selectDateWarning);
+  errorCount += showWarning($dateSelect, "default", $selectDateWarning);
+  errorCount += showWarning(
+    $startHourSelect,
+    "start-default",
+    $selectStartWarning
+  );
+  errorCount += showWarning(
+    $startMinSelect,
+    "start-default",
+    $selectStartWarning
+  );
+  errorCount += showWarning($endHourSelect, "end-default", $selectEndWarning);
+  errorCount += showWarning($endMinSelect, "end-default", $selectEndWarning);
+
+  // 서버로 전송하는 로직
+
+  if (errorCount === 0) {
+    console.log("successful");
+  } else {
+    console.log("validation error");
+  }
+};
