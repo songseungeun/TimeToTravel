bcddb65abf1443de831f4f920e60da67cc0ea084
kfriendship1003 <kfriendship94@gmail.com>
2020-04-21 21:22:21 +0900

    :sparkles: add D-day feature

--- public/js/main.js
+++ public/js/main.js
@@ -52,9 +52,18 @@ const renderTravelList = () => {
   let html = '';
 
   travels.forEach(({ id, title, place, startDate, endDate }) => {
+    const generateDday = () => {
+      let dDay = 0;
+      let today = new Date();
+      today = today.getTime();
+      dDay = new Date(startDate).getTime();
+      dDay = Math.ceil((dDay - today) / 86400000) + 1;
+      return dDay;
+    };
+
     html += ` <li id=${id}>
           <h2>${title}</h2>
-          <em>D-${startDate}</em>
+          <em>D-${generateDday()}</em>
           <div class="travel-info">
             <span class="travel-place">${place}</span>
             <span class="travel-date">${startDate} ~ ${endDate}</span>
@@ -168,7 +177,6 @@ $addTravelBtn.onclick = async () => {
 
   closeTravelPopup();
   renderTravelList();
-  makeScheduleData(newId);
 
   $inputTravelTitle.value = '';
   $inputTravelPlace.value = '';
