454af3e95efeb8b398f2497da7f6271627c41e7a
heejinlee07 <heejinlee07@gmail.com>
2020-04-23 23:20:58 +0900

    still working

--- public/js/menu-icon.js
+++
@@ -1,9 +0,0 @@
-// import { getAirlineData, getLodgingData } from './travel-info.js';
-// import { $menuList, $mainList, $timelineTitle, changeNav } from './main.js';
-
-// $menuList.onclick = ({ target }) => {
-//   if (navState === 'airplane') {
-//     getAirlineData();
-//     getLodgingData();
-//   }
-// };
