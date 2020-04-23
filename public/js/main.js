045a6cfdccd0975718c5846e830031b1b2590d06
kfriendship1003 <kfriendship94@gmail.com>
2020-04-23 11:08:50 +0900

    :bug: fix info page

--- public/js/main.js
+++ public/js/main.js
@@ -44,95 +44,17 @@ const $timelineTitle = document.querySelector('.timeline-travel-title');
 
 const $popupBg = document.querySelector('.popup-bg');
 const $popupRemoveBtn = document.querySelector('.popup-remove-btn');
-const $monthSelect = document.querySelector('#month-select');
-const $dateSelect = document.querySelector('#date-select');
-const $startHourSelect = document.querySelector('#start-hour-select');
-const $startMinSelect = document.querySelector('#start-min-select');
-const $endHourSelect = document.querySelector('#end-hour-select');
-const $endMinSelect = document.querySelector('#end-min-select');
-
-const $inputPlace = document.querySelector('#schedule-input-place');
-const $inputDetail = document.querySelector('#schedule-input-detail');
-const $selectDateWarning = document.querySelector('#date-warning-label');
-const $selectStartWarning = document.querySelector('#start-warning-label');
-const $selectEndWarning = document.querySelector('#end-warning-label');
-const $inputDetailWarning = document.querySelector('#detail-warning-label');
-const $inputPlaceWarning = document.querySelector('#place-warning-label');
 const $deleteConfirmModal = document.querySelector('.delete-confirm-modal');
 
+// functions
+// popups
+//+버튼 누르면 팝업창 오픈
 const closePopup = () => {
   $newSchedulePopUp.style.display = 'none';
   $popupBg.style.display = 'none';
   resetSchedulePopup();
 };
 
-//+버튼 누르면 팝업창 오픈
-$newScheduleBtn.onclick = () => {
-  $newSchedulePopUp.style.display = 'block';
-  $popupBg.style.display = 'block';
-};
-
-//x버튼을 누르면 팝업창 종료
-$popupRemoveBtn.addEventListener('click', closePopup);
-
-/*
-window.onclick = ({ target }) => {
-    if (target === $popupBg || target !== $newScheduleBtn) {
-      closePopup();
-      resetSchedulePopup();
-    }
-    
-    if (target !== $deleteConfirmModal) {
-      $alertPopupBg.style.display = 'none';
-      $alertPopup.style.display = 'none';
-    }
-    
-  };
-*/
-//경고문구
-//FIXME: &&나 ||로 세트인 아이들을 묶어줄 수 없는지?
-/*
-function showWarning(element, defaultValue, warningElement) {
-  if (element.value === defaultValue) {
-    warningElement.classList.remove('text-hidden');
-    return 1;
-  } else {
-    warningElement.classList.add('text-hidden');
-    return 0;
-  }
-}
-
-$addScheduleBtn.onclick = () => {
-  let errorCount = 0;
-  errorCount += showWarning($inputPlace, '', $inputPlaceWarning);
-  errorCount += showWarning($inputDetail, '', $inputDetailWarning);
-  errorCount += showWarning($monthSelect, 'default', $selectDateWarning);
-  errorCount += showWarning($dateSelect, 'default', $selectDateWarning);
-  errorCount += showWarning(
-    $startHourSelect,
-    'start-default',
-    $selectStartWarning
-  );
-  errorCount += showWarning(
-    $startMinSelect,
-    'start-default',
-    $selectStartWarning
-  );
-  errorCount += showWarning($endHourSelect, 'end-default', $selectEndWarning);
-  errorCount += showWarning($endMinSelect, 'end-default', $selectEndWarning);
-
-  // 서버로 전송하는 로직
-
-  if (errorCount === 0) {
-    console.log('successful');
-  } else {
-    console.log('validation error');
-  }
-};
-*/
-
-// functions
-// popups
 const resetSchedulePopup = () => {
   $inputSchedulePlace.value = '';
   $inputScheduleDetail.value = '';
@@ -438,6 +360,21 @@ const goToTimeline = async target => {
 window.onload = getTravels;
 
 $addScheduleBtn.addEventListener('click', addSchedule);
+
+//x버튼을 누르면 팝업창 종료
+$popupRemoveBtn.addEventListener('click', closePopup);
+
+window.onclick = ({ target }) => {
+    if (target === $popupBg || target !== $newScheduleBtn) {
+      closePopup();
+      resetSchedulePopup();
+    }
+  };
+
+$newScheduleBtn.onclick = () => {
+  $newSchedulePopUp.style.display = 'block';
+  $popupBg.style.display = 'block';
+};
 
 $addTravelBtn.onclick = async () => {
   const title = $inputTravelTitle.value.trim();
